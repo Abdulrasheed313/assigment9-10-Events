42bea8616b09313706fca5ea365f6bd8d141c6ba
Abdulrasheed313 <rasheedmumna@gmail.com>
2025-05-12 05:16:09 +0500

    new

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
-// function clicks(){
-//     alert(' Why Your Click !!!!')
-// }
+function clicks(){
+    alert(' Why Your Click !!!!')
+}
 
-// function purc(){
-//     alert(' Why Your Click !!!!')
+function purc(){
+    alert(' Why Your Click !!!!')
 
-// }
+}
 
 
-//   function deleteRow(e){
-//     e.parentElement.parentElement.remove();
+  function deleteRow(e){
+    e.parentElement.parentElement.remove();
     
-//   }
+  }
 
 
-// var count = 0;
-// function changeCounter(val) {
-//   count += val;
-//   document.getElementById('counter').textContent = count;
-// }
+var count = 0;
+function changeCounter(val) {
+  count += val;
+  document.getElementById('counter').textContent = count;
+}
 
-// var inp = document.getElementById('inp');
-// var sub = document.getElementById('sub');
-// var ul = document.getElementById('ul')
+var inp = document.getElementById('inp');
+var sub = document.getElementById('sub');
+var ul = document.getElementById('ul')
 
 
-// function clr(){
-//     ul.innerHTML = "";
-// }
+function clr(){
+    ul.innerHTML = "";
+}
 
-// function editTask(e){
-// console.log(e)
-//     var getValue = prompt("Enter the updated value",e.parentNode.parentNode.firstChild.textContent);
-//     e.parentNode.parentNode.firstChild.textContent = getValue;
-// }
+function editTask(e){
+console.log(e)
+    var getValue = prompt("Enter the updated value",e.parentNode.parentNode.firstChild.textContent);
+    e.parentNode.parentNode.firstChild.textContent = getValue;
+}
 
-// function submit(){
-//     if(inp.value == ""){
-//         alert("Enter Your Value")
-//     }else{
-//         ul.innerHTML += `<li class=" m-2 bg-info text-dark"> ${inp.value} <span> <button class=" btn btn-primary " onclick='editTask(this)' >Edit</button> <button class="btn btn-secondary" onclick="this.parentNode.parentNode.remove()">Delete</button></span> </li>`;
+function submit(){
+    if(inp.value == ""){
+        alert("Enter Your Value")
+    }else{
+        ul.innerHTML += `<li class=" m-2 bg-info text-dark"> ${inp.value} <span> <button class=" btn btn-primary " onclick='editTask(this)' >Edit</button> <button class="btn btn-secondary" onclick="this.parentNode.parentNode.remove()">Delete</button></span> </li>`;
 
-//         inp.value = "";
-//     }
+        inp.value = "";
+    }
    
 
 
 
-// }
+}
 
 function toggleText() {
     var moreText = document.getElementById("moreText");
